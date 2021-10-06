dd0ca71ec5aa9c073396dac5c3c13afdf9f6ad82
kmoschis <57968304+kmoschis@users.noreply.github.com>
2021-10-07 10:09:30 +1030

    Update CheckAndAddLocalAdmins.ps1

--- CheckAndAddLocalAdmins.ps1
+++ CheckAndAddLocalAdmins.ps1
@@ -1,3 +1,6 @@
+# Checks for local admins on a machine, then if that count is less than 5, it adds the current user as an admin
+# Otherwiise, if more than 5, it does nothing / makes a directory (for debugging)
+
 $Admins = Get-LocalGroupMember "Administrators"
 $Admins = $Admins | Select -ExpandProperty Name
 $AdminCount = $Admins.Count

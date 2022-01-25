67139f1d439142cea34b4809162d84ad82555252
kmoschis <kmoschis@gmail.com>
2022-01-25 15:01:06 +1030

    Create CheckUsersPasswordValidity.ps1

---
+++ CheckUsersPasswordValidity.ps1
@@ -0,0 +1,28 @@
+$Users = Import-CSV C:\Temp\users.csv | Where-Object {$_.Username } | Select-Object *
+
+foreach ($User in $Users) {
+    $UserDetails = $User | Select-Object Username, Password
+    $Username = $UserDetails.Username
+    $Password = $UserDetails.Password
+
+    Function Test-ADAuthentication {
+        param(
+            $Username,
+            $Password)
+        
+        (New-Object DirectoryServices.DirectoryEntry "",$Username,$Password).psbase.name -ne $null
+    }
+    $Validity = Test-ADAuthentication -username $UserName -password $Password
+
+    #$Username
+    #$Password
+    #$Validity
+
+    New-Object -TypeName psobject -Property @{
+        Username = $Username
+        Passwrd = $Password
+        Validity = $Validity
+    }
+
+    Add-Content -Value "$($Username),$($Password),$($Validity)" -Path C:\Temp\users-out.csv
+}
